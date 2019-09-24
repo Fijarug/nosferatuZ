cbdbe4a131a88559031a23b8888da9ad279d3d7b
Fijarug <fijarug@gmail.com>
2019-09-24 10:50:40 -0300

    2 files added

---
+++ pages/jogando/jogando.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+import { NavController } from 'ionic-angular';
+
+import { Papel } from '../../entidades/papel';
+
+@Component({
+  selector: 'page-jogando',
+  templateUrl: 'jogando.html'
+})
+export class Jogando {
+
+  constructor(public navCtrl: NavController) {
+  }
+
+
+}
