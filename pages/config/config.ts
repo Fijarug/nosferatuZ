72bf1eccc3c7927b014454b6d36ef8924efa612b
Fijarug <fijarug@gmail.com>
2019-09-26 01:17:09 -0300

    1 file modified

--- pages/config/config.ts
+++ pages/config/config.ts
@@ -20,7 +20,7 @@ export class Configuracao {
   public Bom2: any;
   public Bom3: any;
   public e: any;
-  public resultados = new Map();
+  public papeisPorTimeELevel = new Map();
 
   constructor(public navCtrl: NavController, public navParams: NavParams) {
     this.jogadores = navParams.get('jogadores');
@@ -34,30 +34,8 @@ export class Configuracao {
     this.balancear();
 
     //map to array
-    let results = Array.from(this.resultados.keys());    
-
-    for (var i = 0; i <= results.length; i++) {
-      console.log(results[i].time)
-      // if (results[i].time === this.Mal.time) {
-      //   this.sortearPapeis.push(this.Mal.time);
-      // } else if (results[i].time === this.Bom1.time && results[i].level === this.Bom1.level) {
-      //   this.sortearPapeis.push(this.Bom1.time + this.Bom1.level);
-      // } else if (results[i].time === this.Bom2.time && results[i].level === this.Bom2.level) {
-      //   this.sortearPapeis.push(this.Bom2.time + this.Bom2.level);
-      // } else if (results[i].time === this.Bom3.time && results[i].level === this.Bom3.level) {
-      //   this.sortearPapeis.push(this.Bom3.time + this.Bom3.level);
-      // }
-    }
-
-    console.log(this.resultados.get(this.teste(this.resultados)));
-    for (var i = 0; i <= this.jogadores.length; i++) {
-      this.jogadores[i].papel = this.resultados.get(this.teste(this.resultados));
-    }
-  }
-
-  public teste(collection) {
-    let keys = Array.from(collection.keys());
-    return keys[Math.floor(Math.random() * keys.length)];
+    console.log(this.papeisPorTimeELevel)
+    console.log(this.papeisTimeLevel)
   }
 
   public instanciar() {
@@ -89,11 +67,11 @@ export class Configuracao {
 
   public sortear() {
     let papel = [];
-    this.resultados.set(this.Mal, 0);
-    this.resultados.set(this.Bom1, 0);
-    this.resultados.set(this.Bom2, 0);
-    this.resultados.set(this.Bom3, 0);
-    this.resultados.set(this.e, 0);
+    this.papeisPorTimeELevel.set(this.Mal, 0);
+    this.papeisPorTimeELevel.set(this.Bom1, 0);
+    this.papeisPorTimeELevel.set(this.Bom2, 0);
+    this.papeisPorTimeELevel.set(this.Bom3, 0);
+    this.papeisPorTimeELevel.set(this.e, 0);
 
     for (i = 1; i <= 100; i++) {
       if (this.Mal.probabilidade >= i)
@@ -110,32 +88,32 @@ export class Configuracao {
     for (var i = 0; i < this.jogadores.length; i++) {
       let sorteado = Math.floor((Math.random() * papel.length - 1) + 1);
       if (papel[sorteado] === this.Mal) {
-        this.resultados.set(this.Mal, this.resultados.get(this.Mal) + 1)
+        this.papeisPorTimeELevel.set(this.Mal, this.papeisPorTimeELevel.get(this.Mal) + 1)
       } else if (papel[sorteado] === this.Bom1) {
-        this.resultados.set(this.Bom1, this.resultados.get(this.Bom1) + 1)
+        this.papeisPorTimeELevel.set(this.Bom1, this.papeisPorTimeELevel.get(this.Bom1) + 1)
       } else if (papel[sorteado] === this.Bom2) {
-        this.resultados.set(this.Bom2, this.resultados.get(this.Bom2) + 1)
+        this.papeisPorTimeELevel.set(this.Bom2, this.papeisPorTimeELevel.get(this.Bom2) + 1)
       } else if (papel[sorteado] === this.Bom3) {
-        this.resultados.set(this.Bom3, this.resultados.get(this.Bom3) + 1)
+        this.papeisPorTimeELevel.set(this.Bom3, this.papeisPorTimeELevel.get(this.Bom3) + 1)
       }
     }
-    console.log("Mal: " + this.resultados.get(this.Mal));
-    console.log("Bom1: " + this.resultados.get(this.Bom1));
-    console.log("Bom2: " + this.resultados.get(this.Bom2));
-    console.log("Bom3: " + this.resultados.get(this.Bom3));
+    console.log("Mal: " + this.papeisPorTimeELevel.get(this.Mal));
+    console.log("Bom1: " + this.papeisPorTimeELevel.get(this.Bom1));
+    console.log("Bom2: " + this.papeisPorTimeELevel.get(this.Bom2));
+    console.log("Bom3: " + this.papeisPorTimeELevel.get(this.Bom3));
   }
 
   public balancear() {
     for (var i = 0; i < 100; i++) {
-      if (this.resultados.get(this.Mal) < this.resultados.get(this.Bom3)) {
+      if (this.papeisPorTimeELevel.get(this.Mal) < this.papeisPorTimeELevel.get(this.Bom3)) {
         console.log("Refazendo: time aldeao mto forte");
         console.log("------------------------------------")
         this.sortear();
-      } else if (this.resultados.get(this.Mal) >= (this.jogadores.length / 2)) {
+      } else if (this.papeisPorTimeELevel.get(this.Mal) >= (this.jogadores.length / 2)) {
         console.log("Refazendo: time vampiro mto forte");
         console.log("------------------------------------")
         this.sortear();
-      } else if (this.resultados.get(this.Mal) < 1) {
+      } else if (this.papeisPorTimeELevel.get(this.Mal) < 1) {
         console.log("Refazendo: nenhum vampiro");
         console.log("------------------------------------")
         this.sortear();
