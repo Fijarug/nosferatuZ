3d8893bbbf94a05966dacfcba71df2b5e65f851e
Fijarug <fijarug@gmail.com>
2019-09-29 21:18:59 -0300

    2 files modified

--- pages/home/home.ts
+++ pages/home/home.ts
@@ -9,6 +9,7 @@ import { JogadoresPage } from '../jogadores/jogadores';
 export class HomePage {
 
   public logo = "https://github.com/Fijarug/nosferatuZ/blob/master/src/img/logo.png?raw=true";
+  public teste: boolean = false;
 
   constructor(public navCtrl: NavController) {
 
@@ -16,6 +17,10 @@ export class HomePage {
 
   public comecarJogo(): void {
     this.navCtrl.push(JogadoresPage);
+  }
+
+  public tesete(){
+    this.teste= true;
   }
 
 }
