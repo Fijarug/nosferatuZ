362ffa05931229f2e17a02b93771a8255672fc75
Fijarug <fijarug@gmail.com>
2019-10-01 01:23:59 -0300

    2 files modified

--- pages/jogando/jogando.ts
+++ pages/jogando/jogando.ts
@@ -19,6 +19,13 @@ export class Jogando {
 
   public escolhasDeMorte: Array<Jogador> = new Array();
 
+  public matar: boolean = false;
+  public revelar: boolean = false;
+  public defender: boolean = false;
+  public transformar: boolean = false;
+  public armadilha: boolean = false;
+  public semAcao: boolean = false;
+
   constructor(public navCtrl: NavController, public navParams: NavParams) {
     this.jogadores = navParams.get("jogadores");
     this.jogadorAtual = this.jogadores[this.ordem];
@@ -32,6 +39,20 @@ export class Jogando {
     this.jogadorAtual = this.jogadores[this.ordem];
     this.exibirPapel = false;
     this.exibirFuncaoPapel = true;
+
+    if (this.jogadorAtual.papel.matar) {
+      this.matar = true;
+    } else if (this.jogadorAtual.papel.revelar) {
+      this.revelar = true;
+    } else if (this.jogadorAtual.papel.defender) {
+      this.defender = true;
+    } else if (this.jogadorAtual.papel.transformar) {
+      this.transformar = true;
+    } else if (this.jogadorAtual.papel.armadilha) {
+      this.armadilha = true;
+    } else {
+      this.semAcao = true;
+    }
   }
 
   public proximoJogador() {
@@ -45,14 +66,34 @@ export class Jogando {
     } else {
       this.ordem = this.ordem + 1;
     }
+    this.resetarAcoes();
+  }
+
+  public resetarAcoes() {
+    this.matar = false;
+    this.revelar = false;
+    this.defender = false;
+    this.transformar = false;
+    this.armadilha = false;
+    this.semAcao = false;
   }
 
   public exibirPapelBt() {
     this.exibirPapel = true;
   }
 
-  public selecionar(j : Jogador){
+  public selecionar(j: Jogador) {
+    for (var i = 0; i < this.escolhasDeMorte.length; i++) {
+      this.escolhasDeMorte[i].selecionado = false;
+    }
+    this.escolhasDeMorte = new Array();
     j.selecionado = !j.selecionado;
     this.escolhasDeMorte.push(j);
+  }
+
+  public matarVampiro() {
+    this.buscarProximo();
+    this.exibirPapel = false;
+    this.exibirFuncaoPapel = false;
   }
 }
